3dc631e4b679be80a883598c06adad0f34118fdf
Senthamizh-001 <senthamizh20780@gmail.com>
2025-07-31 11:40:21 +0530

    Fix Netlify deployment structure

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        courses: 'courses.html',
+        about: 'about.html',
+        contact: 'contact.html'
+      }
+    }
+  }
+})
